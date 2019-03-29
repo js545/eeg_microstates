8ab738c6591759dbcae79e63180c3337ae0f9ea9
Jake Son <jake.son@childmind.org>
2019-03-29 15:51:12 -0400

    Calculate microstate statistics and save to csv

--- HBN_analysis.m
+++ HBN_analysis.m
@@ -18,6 +18,7 @@ subjects = dir(datapath);
 %% Load RestingState data
 
 highquality_RS = 0;
+RS_sub_list = [];
 
 for i = 1:length(subjects)
     if strfind(subjects(i).name, 'NDAR') 
@@ -38,6 +39,7 @@ for i = 1:length(subjects)
                 [ALLEEG, EEG, CURRENTSET] = eeg_store( ALLEEG, EEG, 0);
                 eeglab redraw  
                 highquality_RS = highquality_RS+1;
+                RS_sub_list = [RS_sub_list; subjects(i).name];
                 
             elseif exist(RS_file2, 'file')
                 load(RS_file2)
@@ -48,6 +50,7 @@ for i = 1:length(subjects)
                 [ALLEEG, EEG, CURRENTSET] = eeg_store( ALLEEG, EEG, 0);
                 eeglab redraw
                 highquality_RS = highquality_RS+1;
+                RS_sub_list = [RS_sub_list; subjects(i).name];
                 
             end
         end
@@ -90,8 +93,13 @@ figure;MicroPlotTopo( EEG, 'plot_range', [] );
 EEG = pop_micro_selectNmicro( EEG);
 [ALLEEG, EEG] = eeg_store(ALLEEG, EEG, CURRENTSET);
 
+% Save prototypes
+save('/data2/Projects/Jake_matlab/microstates_project/prototypes.mat', 'EEG')
+
 %% Load Video4 data
+
 highquality_Video4 = 0;
+video4_sub_list = [];
 
 for i = 1:length(subjects)
     if strfind(subjects(i).name, 'NDAR')
@@ -100,28 +108,28 @@ for i = 1:length(subjects)
             % Only selects files with "good" quality data, indicated by "g" at
             % beginning of the filename
 
-            Video4_file = strcat(datapath, subjects(i).name, '/bp_Video4.mat');
-            Video4_file2 = strcat(datapath, subjects(i).name, '/bip_Video4.mat');
+            Video4_file = strcat(datapath, subjects(i).name, '/gp_Video4.mat');
+            Video4_file2 = strcat(datapath, subjects(i).name, '/gip_Video4.mat');
 
             if exist(Video4_file, 'file')
                 load(Video4_file)
                 EEG = pop_eegfiltnew(EEG, 1, 0);
                 EEG = pop_eegfiltnew(EEG, 0, 30);
                 EEG = pop_resample(EEG, 125);
-                EEG = pop_selectevent( EEG, 'type',{'20'},'deleteevents','on');
                 [ALLEEG, EEG, CURRENTSET] = eeg_store( ALLEEG, EEG, 0);
                 eeglab redraw  
                 highquality_Video4 = highquality_Video4+1;
+                video4_sub_list = [video4_sub_list; subjects(i).name];
                 
             elseif exist(Video4_file2, 'file')
                 load(Video4_file2)
                 EEG = pop_eegfiltnew(EEG, 1, 0);
                 EEG = pop_eegfiltnew(EEG, 0, 30);
                 EEG = pop_resample(EEG, 125);
-                EEG = pop_selectevent( EEG, 'type',{'20'},'deleteevents','on');
                 [ALLEEG, EEG, CURRENTSET] = eeg_store( ALLEEG, EEG, 0);
                 eeglab redraw
                 highquality_Video4 = highquality_Video4+1;
+                video4_sub_list = [video4_sub_list; subjects(i).name];
                 
             end
         end
@@ -130,13 +138,13 @@ end
 
 disp([num2str(highquality_Video4), ' Video4 files added'])
 
-%% Backfit onto RestingState
+%% Backfit onto RestingState and obtain statistics
 
-for i = 1:num_sub
+for i = 1:highquality_RS
 
     fprintf('Importing prototypes and backfitting for dataset %i\n',i)
     [ALLEEG, EEG, CURRENTSET] = pop_newset(ALLEEG, EEG, CURRENTSET,'retrieve',i,'study',0);
-    EEG = pop_micro_import_proto( EEG, ALLEEG, num_sub+1);
+    EEG = pop_micro_import_proto( EEG, ALLEEG, highquality_RS+1);
 
     EEG = pop_micro_fit( EEG, 'polarity', 0 );
 
@@ -151,7 +159,7 @@ for i = 1:num_sub
 
 end
 
-%% Backfit onto Video4
+%% Backfit onto Video4 and obtain statistics
 
 for i = highquality_RS+2:highquality_RS+1+highquality_Video4
 
@@ -171,6 +179,70 @@ for i = highquality_RS+2:highquality_RS+1+highquality_Video4
     [ALLEEG, EEG] = eeg_store(ALLEEG, EEG, CURRENTSET);
 
 end
+
+%% Save statistics
+
+% Resting State
+GEV_v = [];
+Occurence1 = []; Occurence2 = []; Occurence3 = []; Occurence4 = [];
+Duration1 = []; Duration2 = []; Duration3 = []; Duration4 = [];
+Coverage1 = []; Coverage2 = []; Coverage3 = []; Coverage4 = [];
+
+for i = 1:highquality_RS
+
+    [ALLEEG, EEG, CURRENTSET] = pop_newset(ALLEEG, EEG, CURRENTSET,'retrieve',i,'study',0);
+    eeglab redraw
+    
+    GEVtotal = EEG.microstate.stats.GEVtotal;
+    Occurence = EEG.microstate.stats.Occurence;
+    Duration = EEG.microstate.stats.Duration;
+    Coverage = EEG.microstate.stats.Coverage;
+    GEV = EEG.microstate.stats.GEV;
+    
+    GEV_v = [GEV_v; GEVtotal];
+    Occurence1 = [Occurence1; Occurence(1)]; Occurence2 = [Occurence2; Occurence(2)];
+    Occurence3 = [Occurence3; Occurence(3)]; Occurence4 = [Occurence4; Occurence(4)]; 
+    Duration1 = [Duration1; Duration(1)]; Duration2 = [Duration2; Duration(2)]; 
+    Duration3 = [Duration3; Duration(3)]; Duration4 = [Duration4; Duration(4)]; 
+    Coverage1 = [Coverage1; Coverage(1)]; Coverage2 = [Coverage2; Coverage(2)]; 
+    Coverage3 = [Coverage3; Coverage(3)]; Coverage4 = [Coverage4; Coverage(4)]; 
+
+end
+
+RS_output = [GEV_v Occurence1 Occurence2 Occurence3 Occurence4 Duration1 Duration2 Duration3 Duration4 Coverage1 Coverage2 Coverage3 Coverage4];
+csvwrite('/data2/Projects/Jake_matlab/microstates_project/rs_statistics.csv', RS_output);
+
+% Video4
+GEV_v = [];
+Occurence1 = []; Occurence2 = []; Occurence3 = []; Occurence4 = [];
+Duration1 = []; Duration2 = []; Duration3 = []; Duration4 = [];
+Coverage1 = []; Coverage2 = []; Coverage3 = []; Coverage4 = [];
+
+for i = highquality_RS+2:highquality_RS+1+highquality_Video4
+
+    [ALLEEG, EEG, CURRENTSET] = pop_newset(ALLEEG, EEG, CURRENTSET,'retrieve',i,'study',0);
+    eeglab redraw
+    
+    GEVtotal = EEG.microstate.stats.GEVtotal;
+    Occurence = EEG.microstate.stats.Occurence;
+    Duration = EEG.microstate.stats.Duration;
+    Coverage = EEG.microstate.stats.Coverage;
+    GEV = EEG.microstate.stats.GEV;
+    
+    GEV_v = [GEV_v; GEVtotal];
+    Occurence1 = [Occurence1; Occurence(1)]; Occurence2 = [Occurence2; Occurence(2)];
+    Occurence3 = [Occurence3; Occurence(3)]; Occurence4 = [Occurence4; Occurence(4)]; 
+    Duration1 = [Duration1; Duration(1)]; Duration2 = [Duration2; Duration(2)]; 
+    Duration3 = [Duration3; Duration(3)]; Duration4 = [Duration4; Duration(4)]; 
+    Coverage1 = [Coverage1; Coverage(1)]; Coverage2 = [Coverage2; Coverage(2)]; 
+    Coverage3 = [Coverage3; Coverage(3)]; Coverage4 = [Coverage4; Coverage(4)]; 
+
+end
+
+RS_output = [GEV_v Occurence1 Occurence2 Occurence3 Occurence4 Duration1 Duration2 Duration3 Duration4 Coverage1 Coverage2 Coverage3 Coverage4];
+csvwrite('/data2/Projects/Jake_matlab/microstates_project/video4_statistics.csv', RS_output);
+
+
 
 
 
